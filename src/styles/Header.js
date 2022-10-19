439b3c1e14e2aefa81dde6a72c03ffd258672694
Paulo Serrano <pvsserrano@gmail.com>
2022-10-19 15:58:59 -0300

    header component

---
+++ src/styles/Header.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+const HeaderWrapper = styled.header`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: min(70px, 20vh);
+    position: relative;
+    padding: 0.5rem 1.5rem;
+    margin-bottom: 5rem;
+    background-color: #151515;
+
+    h1{
+        font-family: 'Passion One', cursive;
+        font-weight: bold;
+        font-size: 3rem;
+        cursor: pointer;
+    }
+
+    .user-container{
+        display: flex;
+        align-items: center;
+        height: 100%;
+        margin-left: auto;
+
+        img, button{
+            cursor: pointer;
+        }
+
+        img{
+            height: 100%;
+            margin-left: 0.5rem;
+            border-radius: 50%;
+        }
+
+        button{
+            background-color: transparent;
+            border: none;
+            color: #fff;
+        }
+    }
+`
+
+export { HeaderWrapper }
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,3 +1,12 @@
 import { GlobalStyle } from "./GlobalStyle";
+import { HeaderWrapper } from "./Header";
 
-export { GlobalStyle }
+export { GlobalStyle, HeaderWrapper }
+
+/*
+font-family: 'Lato', sans-serif;
+
+font-family: 'Oswald', sans-serif;
+
+font-family: 'Passion One', cursive;
+*/
