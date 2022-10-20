5ee4b84040e503c1213598dc04ad4bd85eca4d93
aldusd <aldusdanielfsm@gmail.com>
2022-10-20 00:52:47 -0300

    feat: turns login and signup layouts into functional pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,21 +4,44 @@ import { useEffect, useState } from "react";
 import { AuthPagesWrapper, Left } from "../styles/AuthPagesWrapper.js";
 import Logo from "../common/Logo.js";
 import { FormWrapper } from "../styles/FormWrapper.js";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import * as api from "../services/axios.js";
 
 const Login = () => {
     // Logic
+    const navigate = useNavigate();
+
     const [form, setForm] = useState(
         {
             email: '',
             password: '',
         }
     )
+
+    const [buttonBlocked, setButtonBlocked] = useState(false);
+
     const updateForm = field => setForm({ ...form, [field.name]: field.value});
     
-    const login = click => {
+    const tryLogin = async click => {
         click.preventDefault();
-        console.log(form)
+        if(!buttonBlocked) {
+            setButtonBlocked(true);
+            const result = await login();
+        }
+    }
+
+    const login = async () => {
+        try {
+            const signin = await api.login(form);
+            localStorage.setItem("accessToken", signin.data.token);
+            localStorage.setItem("refreshToken", signin.data.refreshToken);
+
+            navigate("/timeline");
+        } catch (error) {
+            setButtonBlocked(false);
+            console.log(error);
+            alert(error.response.data)
+        }
     }
 
     // UI
@@ -29,9 +52,9 @@ const Login = () => {
             </Left>
             <div>
                 <FormWrapper>
-                    <input required type="email" name="email" placeholder="e-mail" value={form.name} onChange={e => updateForm(e.target)} />
-                    <input required type="password" name="password" placeholder="password" value={form.name} onChange={e => updateForm(e.target)} />
-                    <button onClick={login} >Log In</button>
+                    <input required type="email" name="email" placeholder="e-mail" value={form.email} onChange={e => updateForm(e.target)} />
+                    <input required type="password" name="password" placeholder="password" value={form.password} onChange={e => updateForm(e.target)} />
+                    <button onClick={tryLogin} >{(buttonBlocked) ? <>Loading...</> : <>Log In</>}</button>
                 </FormWrapper> 
                 <Link to="/sign-up" >First time? Create an account!</Link>               
             </div>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,23 +4,42 @@ import { useEffect, useState } from "react";
 import { AuthPagesWrapper, Left } from "../styles/AuthPagesWrapper.js";
 import Logo from "../common/Logo.js";
 import { FormWrapper } from "../styles/FormWrapper.js";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import * as api from "../services/axios.js";
 
 const SignUp = () => {
     // Logic
+    const navigate = useNavigate(); 
+
+    const [buttonBlocked, setButtonBlocked] = useState(false);
+
     const [form, setForm] = useState(
         {
             email: '',
             password: '',
-            username: '',
+            name: '',
             pictureUrl: ''
         }
     )
     const updateForm = field => setForm({ ...form, [field.name]: field.value});
     
-    const signup = click => {
+    const trySignup = async click => {
         click.preventDefault();
-        console.log(form)
+        if(!buttonBlocked) {
+            setButtonBlocked(true);
+            const result = await signup();
+        }
+    }
+
+    const signup = async () => {
+        try {
+            const register = await api.signUp(form);
+            navigate("/");
+        } catch (error) {
+            setButtonBlocked(false);
+            console.log(error);
+            alert(error.response.data)
+        }
     }
 
     // UI
@@ -31,11 +50,11 @@ const SignUp = () => {
             </Left>
             <div>
                 <FormWrapper>
-                    <input required type="email" name="email" placeholder="e-mail" value={form.name} onChange={e => updateForm(e.target)} />
-                    <input required type="password" name="password" placeholder="password" value={form.name} onChange={e => updateForm(e.target)} />
-                    <input required type="text" name="username" placeholder="username" value={form.name} onChange={e => updateForm(e.target)} />
-                    <input required type="text" name="pictureUrl" placeholder="picture url" value={form.name} onChange={e => updateForm(e.target)} />
-                    <button onClick={signup} >Sign Up</button>
+                    <input required type="email" name="email" placeholder="e-mail" value={form.email} onChange={e => updateForm(e.target)} />
+                    <input required type="password" name="password" placeholder="password" value={form.password} onChange={e => updateForm(e.target)} />
+                    <input required type="text" name="name" placeholder="username" value={form.name} onChange={e => updateForm(e.target)} />
+                    <input required type="text" name="pictureUrl" placeholder="picture url" value={form.pictureUrl} onChange={e => updateForm(e.target)} />
+                    <button onClick={trySignup} >{(buttonBlocked) ? <>Loading...</> : <>Sign Up</>}</button>
                 </FormWrapper> 
                 <Link to="/" >Switch back to log in</Link>               
             </div>
