c6a72cbd49999983ea00d669ffb933c099b296ac
Pedro Pereira <plp.leoncio@gmail.com>
2022-10-24 17:05:38 -0300

    feat hover like base

--- src/common/LikeContainer.js
+++ src/common/LikeContainer.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getLikes, deleteLike, insertLike } from "../services/axios";
 import { AiFillHeart as FilledHeart } from "react-icons/ai"
 import { AiOutlineHeart as EmptyHeart } from "react-icons/ai"
@@ -8,7 +8,8 @@ export default function LikeContainer ({postId}){
 
   const [isLiked, setIsLiked] = useState(false)
   const [likeAmount, setLikeAmount] = useState(0)
-  console.log(likeAmount)
+  const [likeInfo, setLikeInfo] = useState(false)
+  const [whoLike, setWhoLike] = useState([])
 
   useEffect (()=>{ updateLikes() },[isLiked])
 
@@ -31,6 +32,7 @@ export default function LikeContainer ({postId}){
       .then((res) => {
         setLikeAmount(res.data?.likeAmount)
         setIsLiked(res.data?.isLiked)
+        setWhoLike(res.data?.whoLiked)
       })
       .catch(err => {
         console.error(err)
@@ -38,7 +40,10 @@ export default function LikeContainer ({postId}){
   }
 
   return(
-    <ContainerLikes onClick={() => likeClickBotton()}>
+    <ContainerLikes onClick={() => likeClickBotton()} onMouseEnter={() => setLikeInfo(true)} onMouseLeave={() => setLikeInfo(false)}>
+      <LikeInfoContainer likeStatus={likeInfo}>
+        {(whoLike !== undefined) ? (whoLike.join(", ")):("")}
+      </LikeInfoContainer>
       <div> {(isLiked ? (<FilledHeartStyled/> ):(<EmptyHeartStyled/> ))}</div>
       <span>{likeAmount} likes</span>
     </ContainerLikes>
@@ -49,8 +54,9 @@ const ContainerLikes = styled.div`
   display:flex;
   flex-direction:column;
   align-items:center;
-
   width:50px;
+
+  position:relative;
 
   div{
     display:flex;
@@ -76,4 +82,24 @@ const FilledHeartStyled = styled(FilledHeart)`
 const EmptyHeartStyled = styled(EmptyHeart)`
   color:#FFFFFF;
   font-size:25px;
+`
+const LikeInfoContainer = styled.div`
+  display:${(props) => (props.likeStatus) ? ("flex"):("none")} !important;
+  position:absolute;
+
+  bottom:-30px;
+  width: 169px !important;
+  height: 24px;
+
+  z-index:3;
+
+  background: rgba(255, 255, 255, 0.9);
+  border-radius: 3px;
+
+  font-family: 'Lato';
+  font-weight: 700;
+  font-size: 11px;
+  line-height: 13px;
+
+  color: #505050;
 `
