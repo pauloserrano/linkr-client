1a5726e4ffc3491b3acfc8548dee75d15d82847f
Paulo Serrano <pvsserrano@gmail.com>
2022-10-26 13:51:14 -0300

    layout

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,21 +1,35 @@
-import { useRef, useState } from "react"
+import { useRef, useState, useEffect } from "react"
 import { PostWrapper } from "../styles"
 import LikeContainer from "../common/LikeContainer"
 import EditBody from "../components/EditBody"
 import EditBtn from "../components/EditBtn"
 import DeleteBtn from "../components/DeleteBtn"
+import CommentBtn from "./CommentBtn"
+import { getComments, setComment } from "../services/axios"
 
 const Post = ({ post, userId, handleDelete }) => {
     const { id, pictureUrl, name, link, metaTitle, metaDescription, metaImage } = post
 
     const [isEditable, setIsEditable] = useState(false)
+    const [showComments, setShowComments] = useState(false)
+    const [comments, setComments] = useState([])
     const postRef = useRef()
+
+    useEffect(() => {
+      getComments({ id: post.id })
+        .then(({ data }) => setComments(data))
+        .catch(console.error)
+    }, [])
     
     return (
       <PostWrapper>
           <aside>
             <img className="user-picture" src={pictureUrl} alt={name} />
             <LikeContainer postId={id}/>
+            <CommentBtn 
+              amount={comments.length}
+              onClick={() => setShowComments(prev => !prev)}
+            />
           </aside>
   
           <main ref={postRef}>
@@ -31,7 +45,7 @@ const Post = ({ post, userId, handleDelete }) => {
               <p className="link">{link}</p>
               <img src={metaImage} alt={metaTitle} />
             </a>
-            { userId === post.userId && 
+            {userId === post.userId && 
               <div className="btn-container">
                 <EditBtn 
                     isEditing={isEditable}
@@ -44,6 +58,11 @@ const Post = ({ post, userId, handleDelete }) => {
               </div>
             }
           </main>
+          <footer>
+            {showComments && comments.map(comment => (<>
+              {comment.body}
+            </>))}
+          </footer>
       </PostWrapper>
     )
   }
