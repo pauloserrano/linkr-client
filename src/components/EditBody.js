9f5f94802619c8c6662bf2a3b26540e3ed466bcc
Paulo Serrano <pvsserrano@gmail.com>
2022-10-26 10:04:59 -0300

    field disabled while awaiting, cancel with ESC

--- src/components/EditBody.js
+++ src/components/EditBody.js
@@ -1,27 +1,50 @@
+import { useEffect, useState } from "react"
 import useForm from "../hooks/useForm"
 import { updatePost } from "../services/axios"
 
 const EditBody = ({ post, isEditable, setIsEditable }) => {
-    const [form, handleForm] = useForm({ body: post.body })
+    const [form, handleForm, setForm] = useForm({ body: post.body })
+    const [isDisabled, setIsDisabled] = useState(false)
+
+    useEffect(() => {
+      if (!isEditable) { setForm({ body: post.body}) }
+
+      else { handleEscapeKey() }
+
+    }, [isEditable])
+
+    const handleEscapeKey = () => {
+      document.addEventListener('keydown', (e) => {
+        if(e.key === 'Escape'){
+          setIsEditable(false)
+        }
+      })
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+        setIsDisabled(true)
 
         try {
             await updatePost({ id: post.id, body: form.body })
             post.body = form.body
             setIsEditable(false)
+            setIsDisabled(false)
             
         } catch (error) {
             console.error(error)
-            alert()
+            setIsDisabled(false)
+            alert("An error occured. Your post could not be edited")
         }
     }
 
     return (<>
       { isEditable 
         ? <form onSubmit={handleSubmit}>
-            <input type="text" name="body" value={form.body || ""} onChange={handleForm} />
+            {isDisabled 
+            ? <input type="text" name="body" value={form.body || ""} disabled />
+            : <input type="text" name="body" value={form.body || ""} onChange={handleForm} />
+            }
           </form>
         : post.body && <p className="body">{post.body}</p>
     }
